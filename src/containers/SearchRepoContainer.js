128d0deffdd91c6183426fabb0f3fd0a015ffd5a
sasatoru <woramit.sks@gmail.com>
2019-04-18 14:49:56 +0700

    init

--- src/containers/SearchRepoContainer.js
+++ src/containers/SearchRepoContainer.js
@@ -7,20 +7,23 @@ import { getTopRepos } from 'actions/repos'
 import SearchResults from 'components/SearchResults'
 
 class SearchRepoContainer extends Component {
-  static async getInitialProps ({ store, query }) {
+  static async getInitialProps({ store, query }) {
     let lang = query.lang || 'javascript'
     await store.dispatch(getTopRepos({ lang }))
   }
 
-  componentDidMount () {
+  componentDidMount() {
     let { getTopRepos } = this.props
     getTopRepos({ lang: 'ruby' })
   }
 
-  render () {
+  render() {
     let { repos } = this.props
     return (
       <Fragment>
+        <Helmet>
+          <title>Turbo Todo</title>
+        </Helmet>
         <div onClick={this._goToAbout}>
           GO TO ABOUT (with <code>router</code>)
         </div>
@@ -34,7 +37,7 @@ class SearchRepoContainer extends Component {
   }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     repos: state.repos
   }
